259530b202605e6d7e5ba7413b0ab80133ee0028
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2024-12-17 13:02:48 +0100

    upp

--- Test.py
+++ Test.py
@@ -1,49 +1,53 @@
-print ("hello, World!")
-print ("tyst Fayaz, kram")
-print ("hej, mitt namn är Viktoria")
-print ("wow!")
+# print ("hello, World!")
+# print ("tyst Fayaz, kram")
+# print ("hej, mitt namn är Viktoria")
+# print ("wow!")
 
-name = "Viktoria"
-age = 29
-print (f"Welcome, {name}! You are {age} years old.")
+# name = "Viktoria"
+# age = 29
+# print (f"Welcome, {name}! You are {age} years old.")
 
 
-x = 10
-y = 20
-z = (x + y)
-print (f"Summan av x och y är: {z}")
+# x = 10
+# y = 20
+# z = (x + y)
+# print (f"Summan av x och y är: {z}")
 
-heltal = 10
-flyttal = 3.14
-text = "Python är roligt"
-sanningsvärde = True
+# heltal = 10
+# flyttal = 3.14
+# text = "Python är roligt"
+# sanningsvärde = True
 
-print(type(heltal))
-print(type(flyttal))
-print(type(text))
-print(type(sanningsvärde))
+# print(type(heltal))
+# print(type(flyttal))
+# print(type(text))
+# print(type(sanningsvärde))
 
-x = 42
-y = str(x)
-print (y)
-print (type(y))
+# x = 42
+# y = str(x)
+# print (y)
+# print (type(y))
 
-x = "123"
-y = 2
-z = int(x)
-print (z * y)
+# x = "123"
+# y = 2
+# z = int(x)
+# print (z * y)
 
 # +, -, *, /, //, %, **
-x = 10
-y = 3
-print (x + y)
-print (x - y)
-print (x * y)
-print (x / y)
-print (x // y)
-print (x % y)
-print (x ** y)
+# x = 10
+# y = 3
+# print (x + y)
+# print (x - y)
+# print (x * y)
+# print (x / y)
+# print (x // y)
+# print (x % y)
+# print (x ** y)
 
-pi = 3.14
-x = int(pi)
+# pi = 3.14
+# x = int(pi)
+# print (y)
+
+x = 5
+y = float(x)
 print (y)
