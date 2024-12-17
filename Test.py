9d8bf9ff473fd99600e754a39223620afe748692
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2024-12-17 10:09:55 +0100

    Summa

--- Test.py
+++ Test.py
@@ -1,4 +1,13 @@
-print ("hello, World!")
-print ("tyst Fayaz, kram")
-print ("hej, mitt namn är Viktoria")
-print ("wow!")
+# print ("hello, World!")
+# print ("tyst Fayaz, kram")
+# print ("hej, mitt namn är Viktoria")
+# print ("wow!")
+
+# name = "Viktoria"
+# age = 29
+# print (f"Welcome, {name}! You are {age} years old.")
+
+x = 10
+y = 20
+z = (x + y)
+print (f"Summan av x och y är: {z}")
