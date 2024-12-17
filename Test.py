1a747156e9b1f3b42df37f23fe1417067739e372
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2024-12-17 10:32:51 +0100

    upp

--- Test.py
+++ Test.py
@@ -7,7 +7,17 @@
 # age = 29
 # print (f"Welcome, {name}! You are {age} years old.")
 
-x = 10
-y = 20
-z = (x + y)
-print (f"Summan av x och y är: {z}")
+# x = 10
+# y = 20
+# z = (x + y)
+# print (f"Summan av x och y är: {z}")
+
+heltal = 10
+flyttal = 3.14
+text = "Python är roligt"
+sanningsvärde = True
+
+print(type(heltal))
+print(type(flyttal))
+print(type(text))
+print(type(sanningsvärde))
