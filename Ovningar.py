8f2fcb68b4b18b2fcba6a9ac2d16c59094253671
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2024-12-18 13:50:18 +0100

    up

--- Ovningar.py
+++ Ovningar.py
@@ -14,3 +14,15 @@ print(cities)
 #Skriv ut alla städer med en for-loop
 for index, cities in enumerate(cities, start = 1):
     print(f"{index}. {cities}")
+
+#Skapa en lista med 5 heltal
+heltal = [1, 2, 3, 4, 5]
+print(heltal)
+
+#Summera alla tal i listan
+summa = sum(heltal)
+print(summa)
+
+#Skapa en lista med namn och sök efter ett namn med en for-loop
+namnLista = ["Anna", "Peter", "Saga", "Elsa", "Martin"]
+print(namnLista[2])
