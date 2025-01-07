073ad6ee7ff26e74c03e6ee1706a09d36b59c396
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-07 14:00:33 +0100

    upp

--- inlupp/uppgift-6/uppgift_6.py
+++ inlupp/uppgift-6/uppgift_6.py
@@ -1,8 +1,18 @@
 # Uppgift 6
 # Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def multiplication_table(n, limit):
+    reslutat = []
+    for i in range (1, limit + 1):
+        reslutat.append(n * i)
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    return reslutat
+n = 2
+limit = 3
+print(multiplication_table(n, limit))
+
+
+
+
