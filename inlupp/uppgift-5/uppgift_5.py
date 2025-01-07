8cdbb11e62b68d5bd1f231510ac53441c0342ced
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-07 11:41:23 +0100

    uppgift4

--- inlupp/uppgift-5/uppgift_5.py
+++ inlupp/uppgift-5/uppgift_5.py
@@ -1,8 +1,10 @@
 # Uppgift 5
 # Skapa en funktion filter_odd(numbers) som returnerar en lista med alla jämna tal från den givna listan.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def filter_odd(numbers):
+    return [numbers for numbers in numbers if numbers % 2 == 0] 
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+numbers = [1, 2, 3, 4]
+print(filter_odd(numbers))
