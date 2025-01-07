d01219bdcc7b7141909727fc73638b5b280e3288
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-07 14:48:17 +0100

    uppgift7

--- inlupp/uppgift-7/uppgift_7.py
+++ inlupp/uppgift-7/uppgift_7.py
@@ -1,8 +1,18 @@
 # Uppgift 7
 # Skapa en funktion validate_password(password) som kontrollerar att lösenordet är minst 8 tecken långt och innehåller minst en siffra.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def validate_password(password):
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    if len(password) < 8:
+        return False
+
+    elif not any (char.isdigit() for char in password):
+        return False
+    
+    return True
+
+print(validate_password("abc12345"))
+print(validate_password("short"))
+print(validate_password("password1"))
