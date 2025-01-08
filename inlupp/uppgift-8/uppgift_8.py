038be29cb79c45e497330de2cc53b0d69e983b30
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-08 12:05:14 +0100

    uppgift8

--- inlupp/uppgift-8/uppgift_8.py
+++ inlupp/uppgift-8/uppgift_8.py
@@ -2,12 +2,14 @@
 # Skapa en funktion count_letters(string) som returnerar en dictionary med varje bokstav som nyckel och antalet förekomster som värde.
 
 def count_letters(string):
-    """
-    Skriv beskrivning här.
-    """
-    letterCount = []
-     = {}
-    for i in s:
-    s[i] = s.get(i,0)+1
-    return s
+    letter_counts = {}  
+    for char in string:
+            if char in letter_counts:  
+                letter_counts[char] += 1  
+            else:
+                letter_counts[char] = 1  
+    return letter_counts  
 
+print(count_letters("hello")) #output {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+print(count_letters("")) #output {}
+print(count_letters("aaa")) #output {'a': 3}
