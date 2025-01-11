887d36334d0bcd75fb906656cf3ba5321c14bd7e
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-11 13:48:35 +0100

    uppgift10

--- inlupp/uppgift-10/uppgift_10.py
+++ inlupp/uppgift-10/uppgift_10.py
@@ -1,8 +1,12 @@
 # Uppgift 10
 # Skapa en funktion celsius_to_fahrenheit(celsius) som konverterar en temperatur från Celsius till Fahrenheit.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def celsius_to_fahrenheit(celsius):
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    fahrenheit = celsius * (9 / 5) + 32
+    return fahrenheit
+print(celsius_to_fahrenheit(0))
+print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(-40))
