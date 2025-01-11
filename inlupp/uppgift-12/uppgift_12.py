cd7b5ebb1f0b8609b7d500cae72bb5070d4c77bd
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-11 14:51:36 +0100

    uppgift12

--- inlupp/uppgift-12/uppgift_12.py
+++ inlupp/uppgift-12/uppgift_12.py
@@ -1,8 +1,19 @@
 # Uppgift 12
 # Skapa en funktion create_student_register(students) som tar emot en lista med namn och ålder och returnerar en dictionary där namnet är nyckeln och åldern är värdet.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def create_student_register(students):
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    student_register = {}
+
+    for student in students:
+        name, age = student 
+        student_register[name] = age
+
+    return student_register
+
+students = [("Anna", 20), ("Bertil", 25), ("Cecilia", 22)]
+student_register = create_student_register(students)
+
+print(student_register)
