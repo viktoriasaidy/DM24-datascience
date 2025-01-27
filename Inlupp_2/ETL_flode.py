070f8e0449fb9e89aecc39f26de20bb8fc562d75
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-27 11:24:28 +0100

    Inlupp2

--- Inlupp_2/ETL_flode.py
+++ Inlupp_2/ETL_flode.py
@@ -1,7 +1,22 @@
 # Läsa in CSV filen
 import pandas as pd
 
-file_path = "C:\\Users\\vikto\\Downloads\\Valdeltagande_förstagångsväljare_2004_2014 (1).csv"
+file_path = "C:\\Users\\vikto\\OneDrive\\Dokument\\DATA MANAGER 2024\\Data science\\Inlupp_2\\andel-matavfall-som-behandlas-biologiskt.csv"
 
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path, sep=";")
 print(df)
+
+# Hantera saknade värden
+# Se ifall det finns saknade värden
+print(df.isnull().values.any()) # Output: True
+
+# Radera rader med saknade värden
+print("drop")
+df = df.dropna()
+print(df)
+
+# # Skapa ny kolumn baserat på beräkningar eller villkor.
+# df["Total röster"] = df[""] + df["2010"] + df["2006"]
+# print(df)
+
+# print(df.columns)
