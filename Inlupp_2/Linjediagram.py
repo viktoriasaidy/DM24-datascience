7718cb34dec06e7f3390fe499372cdaa3d096b82
viktoriasaidy <viktoria.saidy.data24sto@edu.tucsweden.se>
2025-01-28 09:00:58 +0100

    Linjediagram

---
+++ Inlupp_2/Linjediagram.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+file_path = "C:\\Users\\vikto\\OneDrive\\Dokument\\DATA MANAGER 2024\\Data science\\Inlupp_2\\andel-matavfall-som-behandlas-biologiskt.csv"
+
+df = pd.read_csv(file_path, sep=";")
+print(df)
+
+matavfall_per_år = df.groupby('År')['Andel matavfall som behandlas biologiskt, %'].sum().reset_index()
+print(matavfall_per_år)
+
+plt.figure(figsize=(10, 6))
+plt.plot(matavfall_per_år['År'], matavfall_per_år['Andel matavfall som behandlas biologiskt, %'])
+
+# Lägg till etiketter och titel
+plt.xlabel('År')
+plt.ylabel('Andel matavfall som behandlas biologiskt, %')
+plt.title('Andel matavfall behandlat biologiskt per år')
+plt.show()
